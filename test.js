9b1525321d707056ab26c11f33849bdd905c78b5
Jacob Van Schenck <jacob.vanschenck@gmail.com>
2022-05-12 14:42:14 -0500

    updates test.js

--- test.js
+++ test.js
@@ -8,13 +8,13 @@ const {
 function main() {
     const tree = getMerkleTree()
     const root = getMerkleRoot(tree)
-    const leaf = getMerkleLeaf('0xb59c13A72c334d3d9794F6b676524c3f19f03c16')
+    const leaf = getMerkleLeaf('0x2B602B851C5A337fb3cf451B2ec4e8433Da185B3')
     const proof = getMerkleProof(tree, leaf)
 
     console.log(
         `root: ${root}\nproof: ${proof.map(
             (item) => '0x' + item.data.toString('hex')
-        )}\naddress: 0xb59c13A72c334d3d9794F6b676524c3f19f03c16\n`
+        )}\naddress: 0x2B602B851C5A337fb3cf451B2ec4e8433Da185B3\n`
     )
 
     console.log(tree.verify(proof, leaf, root))
